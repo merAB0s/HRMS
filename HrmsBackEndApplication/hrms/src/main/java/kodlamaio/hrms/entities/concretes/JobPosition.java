e03916d1b4f3c49968ba9963dd46eb8de5384fc6
Abdullah Aksoy <merab0aks0y@gmail.com>
2021-05-13 01:24:26 +0300

    HRMS BackEnd

---
+++ HrmsBackEndApplication/hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,31 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Data = getter,setter
+@Entity // Entity = implements Entity kısaltması
+@Table(name="job_positions") // Database table 
+@NoArgsConstructor // constructer bos
+@AllArgsConstructor // constructer hepsi
+
+public class JobPosition {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="title")
+	private String title;
+
+}
