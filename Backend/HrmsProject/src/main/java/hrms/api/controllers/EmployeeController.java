00fdc17b880e6968902a1f6c789b5c3f2ddd5a1f
Abdullah Aksoy <merab0aks0y@gmail.com>
2021-07-07 11:23:32 +0300

    1

--- Backend/HrmsProject/src/main/java/hrms/api/controllers/EmployeeController.java
+++ Backend/HrmsProject/src/main/java/hrms/api/controllers/EmployeeController.java
@@ -4,11 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import hrms.business.abstracts.EmployeeService;
 import hrms.core.utilities.results.DataResult;
@@ -17,6 +13,7 @@ import hrms.entities.concretes.Employee;
 
 import javax.validation.Valid;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
--- Backend/HrmsProject/src/main/java/hrms/api/controllers/JobAdvertisementController.java
+++ Backend/HrmsProject/src/main/java/hrms/api/controllers/JobAdvertisementController.java
@@ -5,13 +5,7 @@ import java.util.List;
 import hrms.entities.dtos.JobAdvertisementAndEmployerDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import hrms.business.abstracts.JobAdvertisementService;
 import hrms.core.utilities.results.DataResult;
@@ -20,6 +14,7 @@ import hrms.entities.concretes.JobAdvertisement;
 
 import javax.validation.Valid;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/jobAdvertisements")
 public class JobAdvertisementController {
